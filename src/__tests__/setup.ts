4472c442df665e9437193302bf2b7fcbb89f9024
3thernitydev <3thernitydev@gmail.com>
2025-12-14 17:29:44 +0400

    style: améliorer l'indentation et la lisibilité du fichier de configuration Jest

--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-require-imports, no-empty */
 /**
  * Jest setup file
  */
@@ -14,43 +15,41 @@ process.env.FRONTEND_URL = 'http://localhost:5173';
 process.env.ALLOWED_ORIGINS = 'http://localhost:3000,http://localhost:5173';
 process.env.REDIRECT_URI = 'http://localhost:3000/api/auth/microsoft/callback';
 
-
-
 // Mock global de Prisma pour tous les chemins d'import possibles
 try {
-	jest.mock('../../config/bdd', () => ({
-		__esModule: true,
-		default: require('./mocks/prisma.mock').prismaMock,
-	}));
+    jest.mock('../../config/bdd', () => ({
+        __esModule: true,
+        default: require('./mocks/prisma.mock').prismaMock,
+    }));
 } catch {}
 try {
-	jest.mock('../config/bdd', () => ({
-		__esModule: true,
-		default: require('./mocks/prisma.mock').prismaMock,
-	}));
+    jest.mock('../config/bdd', () => ({
+        __esModule: true,
+        default: require('./mocks/prisma.mock').prismaMock,
+    }));
 } catch {}
 try {
-	jest.mock('src/config/bdd', () => ({
-		__esModule: true,
-		default: require('./mocks/prisma.mock').prismaMock,
-	}));
+    jest.mock('src/config/bdd', () => ({
+        __esModule: true,
+        default: require('./mocks/prisma.mock').prismaMock,
+    }));
 } catch {}
 try {
-	jest.mock('<rootDir>/src/config/bdd', () => ({
-		__esModule: true,
-		default: require('./mocks/prisma.mock').prismaMock,
-	}));
+    jest.mock('<rootDir>/src/config/bdd', () => ({
+        __esModule: true,
+        default: require('./mocks/prisma.mock').prismaMock,
+    }));
 } catch {}
 try {
-	jest.mock('@/config/bdd', () => ({
-		__esModule: true,
-		default: require('./mocks/prisma.mock').prismaMock,
-	}));
+    jest.mock('@/config/bdd', () => ({
+        __esModule: true,
+        default: require('./mocks/prisma.mock').prismaMock,
+    }));
 } catch {}
 
 // Forcer le reset des modules avant chaque test pour garantir l'application du mock
 beforeEach(() => {
-	jest.resetModules();
+    jest.resetModules();
 });
 
 // Set timeout
